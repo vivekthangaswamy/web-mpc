58b797efd8c5d85ceb8a297c3e67458c51ec7570
Kinan Dak Albab <Kinan.bab.14@gmail.com>
2019-05-30 15:46:24 -0400

    Update JIFF and unmasking view clean up

--- client/app/views/unmaskView.js
+++ client/app/views/unmaskView.js
@@ -25,7 +25,6 @@ define(['jquery', 'controllers/jiffController', 'controllers/tableController', '
       }
 
       if (f) {
-
         var keyReader = new FileReader();
         keyReader.readAsText(f);
 
@@ -35,10 +34,7 @@ define(['jquery', 'controllers/jiffController', 'controllers/tableController', '
           var privateKey = e.target.result;
 
           jiffController.analyst.computeAndFormat(sessionKey, sessionPass, privateKey, error, function (result) {
-
-            analystController.getExistingCohorts(sessionKey, sessionPass)
-            .then(function(cohortMapping) {
-              
+            analystController.getExistingCohorts(sessionKey, sessionPass).then(function(cohortMapping) {
               tableController.saveTables(result['averages'], sessionKey, 'Averages', result['cohorts'], cohortMapping);
               tableController.saveTables(result['deviations'], sessionKey, 'Standard_Deviations', result['cohorts'], cohortMapping);
             });
@@ -63,13 +59,9 @@ define(['jquery', 'controllers/jiffController', 'controllers/tableController', '
       var expand_button = $('#expand-table-button');
 
       $(expand_button).click(function () {
-
         var ta = $('#tables-area');
-
         if (ta.css('display') === 'none') {
           ta.show();
-          var maxWidth = $('.wtHider').width() + 50;
-          tableController.updateTableWidth(maxWidth)
         } else {
           ta.hide();
           tableController.resetTableWidth()
